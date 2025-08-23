f0e62affe66afdd12fe36cdf3f0cb757156788de
LATHWALROCK <121502183+LATHWALROCK@users.noreply.github.com>
2025-08-24 01:00:05 +0530

    Update Team.jsx

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -2,12 +2,14 @@ import React from "react";
 
 const Team = ({ image }) => {
   return (
-    <img
-      src={image}
-      alt="team"
-      className="bg-white rounded-xl  h-20 w-auto object-contain 
-                 hover:shadow-lg hover:scale-105 transition-all duration-300 cursor-pointer"
-    />
+    <div className="flex justify-center items-center">
+      <img
+        src={image}
+        alt="team"
+        className="bg-white rounded-xl  h-20 w-auto object-contain 
+              hover:shadow-lg hover:scale-105 transition-all duration-300 cursor-pointer"
+      />
+    </div>
   );
 };
 
