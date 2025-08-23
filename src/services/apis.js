7a88303c541413099d4eab2843925beb5b85bea3
LATHWALROCK <121502183+LATHWALROCK@users.noreply.github.com>
2025-08-24 00:27:53 +0530

    Update apis.js

--- src/services/apis.js
+++ src/services/apis.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:4000/api/v1"
+const BASE_URL = "https://lathwalsports-backend.onrender.com/api/v1"
 
 export const sportEndpoints = {
   CREATE_SPORT: BASE_URL + "/post/createSport",
