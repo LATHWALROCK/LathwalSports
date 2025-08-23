7ff16eb33ddee1a9bcecd0fe0958b31fd4bb5605
LATHWALROCK <121502183+LATHWALROCK@users.noreply.github.com>
2025-08-24 00:56:20 +0530

    Update Team.jsx

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -5,7 +5,7 @@ const Team = ({ image }) => {
     <img
       src={image}
       alt="team"
-      className="bg-white shadow rounded-xl  h-20 w-auto object-contain 
+      className="bg-white rounded-xl  h-20 w-auto object-contain 
                  hover:shadow-lg hover:scale-105 transition-all duration-300 cursor-pointer"
     />
   );
